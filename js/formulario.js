6f25a03df6cbdf23b6ce359e49576d02bde688b8
Washington Lara <washingtonlara35@gmail.com>
2021-01-21 10:27:37 -0500

    21/01/2020 Primera version del Programa

---
+++ js/formulario.js
@@ -0,0 +1,90 @@
+//// washington lara
+
+    $(document).on("click","#btn_register",function(){
+      
+      let n=$("#nombre").val();
+      let ln=$("#lastname").val();
+      let u=$("#user").val();
+      let p=$("#password").val();
+      let ad=$("#address").val();
+      
+      if(n.trim().length<4){ 
+        $("#nombre").select();
+        Swal.fire({
+          icon: 'error',
+          title: 'Falta el nombre',
+        })
+        return false;
+                  }
+              if(ln.trim().length<4){
+                $("#lastname").select();
+                  Swal.fire({
+                      icon: 'error',
+                      title: 'Falta el apellido',
+                    }) 
+                    return false; 
+              } 
+              if(u.trim().length<4){
+                $("#user").select();
+                  Swal.fire({
+                      icon: 'error',
+                      title: 'Falta el usuario',
+                    })  
+                    return false;    
+              } 
+              if(p.trim().length<4){
+                $("#password").select();
+                  Swal.fire({
+                      icon: 'error',
+                      title: 'falta contraseña',
+                    })  
+                    return false;
+              } 
+              if(ad.val()){
+                $("#address").select();
+                  Swal.fire({
+                      icon: 'error',
+                      title: 'Elija una ciudad',
+                    }) 
+                    return true;
+              }  
+              Swal.fire({
+                  icon: 'success',
+                  title: 'registro correcto',
+                }) 
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                  /// washington lara
